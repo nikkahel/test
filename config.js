054bbdcc071b12c8f0a487bafbe61b7f22f0e461
nikkahel <n.k.copy1@gmail.com>
2023-07-24 11:24:14 +0300

    first

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    clientID: '9dfba6451afdf08e',
+    clientSecret: 'd0065075df4e1f236e16de7addc48e6cdabe596e',
+    callbackURL: 'http://localhost:3000/auth/pipedrive/callback'
+};
